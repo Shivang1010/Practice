6a35c6f56fbe2cf23bcf393295bce107245cd3ae
Shivang Srivastava <55347605+Shivang1010@users.noreply.github.com>
2020-09-02 19:41:21 +0530

    Create P11.py

---
+++ ProjectEuler/P11.py
@@ -0,0 +1,24 @@
+max_sum = 0
+for j in range(20):
+    for i in range(20):
+        if j<17: 
+            a = grid[i][j] * \
+            grid[i][j+1] * \
+            grid[i][j+2] * \
+            grid[i][j+3]
+        if i<17: 
+            b = grid[i][j] * \
+            grid[i+1][j] * \
+            grid[i+2][j] * \
+            grid[i+3][j]
+        if i<17 and j<17:
+            c = grid[i][j] * \
+            grid[i+1][j+1] * \
+            grid[i+2][j+2] * \
+            grid[i+3][j+3]
+            d = grid[i][19-j] * \
+            grid[i+1][18-j] * \
+            grid[i+2][17-j] * \
+            grid[i+3][16-j]
+        if max(a,b,c,d) > max_sum: max_sum = max(a,b,c,d)
+print(max_sum)
